a8c8969c70c5c3f75f833c0bc060ec07ebebe291
mtaew <mtae122@gmail.com>
2020-11-02 17:22:52 -0600

    Singly linked list

---
+++ LinkedListMain.java
@@ -0,0 +1,62 @@
+class LinkedListMain {
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+		sll.insertFirst(1);
+		sll.insertFirst(2);
+		sll.insertFirst(3);
+		sll.insertFirst(4);
+        sll.printLinkedList();
+        // 4 => 3 => 2 => 1 =>
+        
+        sll.deleteFirst();
+        sll.printLinkedList();
+        // 3 => 2 => 1 =>
+
+        sll.deleteFirst();
+        sll.deleteFirst();
+        sll.printLinkedList();
+        // 1 =>
+
+        sll.deleteFirst();
+        sll.printLinkedList();
+        // 
+
+        System.out.println(sll.isEmpty());
+        // true
+
+        sll.insertFirst(1);
+        sll.insertLast(4);
+        sll.insertFirst(2);
+        sll.printLinkedList();
+        // 2 => 1 => 4 =>
+
+        System.out.println(sll.isEmpty());
+        // false
+
+        Node n1 = new Node();
+        n1.data = 1;
+        sll.deleteAfter(n1);
+        sll.printLinkedList();
+        // 2 => 1 =>
+        sll.deleteFirst();
+        sll.deleteFirst();
+        sll.printLinkedList();
+
+        sll.insertFirst(1);
+        sll.insertFirst(2);
+        sll.insertFirst(3);
+        sll.insertFirst(4);
+        sll.insertFirst(5);
+    
+        Node n2 = new Node();
+        n2.data = 4;
+        sll.deleteAfter(n2);
+        sll.printLinkedList();
+        // 5 => 4 => 2 => 1 =>
+
+        sll.insertLast(100);
+        sll.printLinkedList();
+        // 5 => 4 => 2 => 1 => 100
+
+    }
+}
---
+++ SinglyLinkedList.java
@@ -0,0 +1,70 @@
+class Node { 
+    int data; 
+    Node next; 
+	public void displayNodeData() {
+		System.out.print(data + " => ");
+	}
+} 
+
+public class SinglyLinkedList {
+	private Node head;
+ 
+	public boolean isEmpty() {
+		return (head == null);
+	}
+    
+    // Insert first node
+	public void insertFirst(int data) {
+		Node n = new Node();
+		n.data = data;  // Set the new node with the given data
+		n.next = head;  // Set the new node to point at the head
+		head = n;       // Set the new node as the head
+	}
+    
+    // Delete first node
+	public Node deleteFirst() {
+		Node n = head;
+		head = head.next; // Head is now the next node removing the very first node
+		return n;
+	}
+    
+    // Delete after given node
+	public void deleteAfter(Node node) {
+        Node n = head;
+        
+        // We don't want whats pointing to be null and make sure it's not the head. Remember it is being deleted AFTER the given node
+		while (n.next != null && n.data != node.data) {
+            n = n.next;
+            System.out.println("Matching node: " + n.data);
+        }
+        
+        // At this point we have the matching node. Since we are deleting *after* it, point it two nodes to the right
+		if (n.next != null) {
+            n.next = n.next.next;
+        }
+	}
+    
+    // Insert node at the end
+	public void insertLast(int data) {
+        Node n = head;
+        
+        // Get to the last node
+		while (n.next != null) {
+			n = n.next;
+        }
+
+		Node newN = new Node();
+		newN.data = data; // Make new node and input the data
+		n.next = newN; // Set the point to the new node
+	}
+    
+    // To => print => that => linked => list => 
+	public void printLinkedList() {
+		Node n = head;
+		while (n != null) {
+			n.displayNodeData();
+			n = n.next;
+		}
+		System.out.println();
+	}
+}
