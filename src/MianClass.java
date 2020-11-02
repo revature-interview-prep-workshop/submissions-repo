cdda0bcd902d37672f52a888f22a3704e4cbab4d
YuanfnegGu <yuanfenggu@hotmail.com>
2020-11-02 16:04:59 -0600

    push & peek

--- src/MianClass.java
+++ src/MianClass.java
@@ -4,6 +4,24 @@ public class MianClass {
 
     public static void main(String[] args) {
         LinkedList<String> team = new LinkedList<>();
+        LinkedList<String> test = new LinkedList<>();
+
+        test.push("a");
+        test.push("v");
+        test.push("a");
+        test.push("J");
+
+        System.out.println(test.toString());
+
+        test.forEach(System.out::print);
+
+        System.out.println(" ");
+
+        System.out.println(test.peek());
+        test.forEach(System.out::print);
+
+        System.out.println(" ");
+
 
         team.add("member_1");
         team.add("member_2");
@@ -15,9 +33,13 @@ public class MianClass {
         team.addLast("member_3");
         System.out.println(team.getLast());
 
+
+
         team.clear();
 
         System.out.println(team);
+
+
 
     }
 }
