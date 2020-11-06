eef46769394301e0e4b9f2500d59e93b4d24baa0
mtaew <mtae122@gmail.com>
2020-11-06 16:27:06 -0600

    Generic HashTable implementation

--- HashTable/HashTable.java
+++ HashTable/HashTable.java
@@ -18,48 +18,48 @@ class HashTable<K, V> {
             return key.toString() + "=" + value.toString();
         }
     } 
-	
-	public HashTable(int size) {
-		this.size = size;
+    
+    public HashTable(int size) {
+        this.size = size;
         hashMap = new ArrayList<>(size);
         
-		for(int i=0; i < size; i++) {
+        for(int i=0; i < size; i++) {
             hashMap.add(i, new ArrayList<HashEntry<K, V>>());
         }
-	}
+    }
 
     public int getHashIndex(K key) {
-		return key.hashCode() % size;
-	}
+        return key.hashCode() % size;
+    }
 
     public void put(K key, V value) {
-		int idx = getHashIndex(key);	
-		ArrayList<HashEntry<K, V>> entryList = hashMap.get(idx);
-		HashEntry<K, V> newEntry = new HashEntry<K, V>(key, value);
+        int idx = getHashIndex(key);	
+        ArrayList<HashEntry<K, V>> entryList = hashMap.get(idx);
+        HashEntry<K, V> newEntry = new HashEntry<K, V>(key, value);
         boolean isFound = false;
         
-		for (HashEntry<K, V> entry : entryList) {
-			if (entry.key.equals(newEntry.key)) {
-				isFound = true;
-				entry.value = newEntry.value;
-			}
+        for (HashEntry<K, V> entry : entryList) {
+            if (entry.key.equals(newEntry.key)) {
+                isFound = true;
+                entry.value = newEntry.value;
+            }
         }
         
-		if (!isFound) {
-			entryList.add(newEntry);
-		}
-	}
+        if (!isFound) {
+            entryList.add(newEntry);
+        }
+    }
 
     public V get(K key) {
-		int idx = getHashIndex(key);
+        int idx = getHashIndex(key);
         ArrayList<HashEntry<K, V>> entryList = hashMap.get(idx);
         
-		for (HashEntry<K, V> entry : entryList) {
-			if(entry.key.equals(key)) {
-				return entry.value;
-			}
-		}
-		return null;
+        for (HashEntry<K, V> entry : entryList) {
+            if(entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+        return null;
     }
 }
 
